3b1cae1051d1526b28868c6285c06703d6fe46a1
Juliannas-003 <202416360003@ifba.edu.br>
2026-06-06 21:07:38 -0300

    Conserto função de cadastro

--- core/views.py
+++ core/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from .forms import UsuarioCreationForm
 
 def cadastro(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UsuarioCreationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Conta criada com sucesso! Faça login.')
             return redirect('/accounts/login/')
     else:
-        form = UserCreationForm()
+        form = UsuarioCreationForm()
     return render(request, 'core/cadastro.html', {'form': form})
-
 
 def home(request):
     return render(request, 'core/home.html')
